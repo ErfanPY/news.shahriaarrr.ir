b39276ba0009958e911121dcf78fa216c5fb57cb
Erfan Ghofrani <erfan.ghofrani.ch@gmail.com>
2025-07-18 02:25:23 +0330

    Update layout view title and navbar text to 'گربه‌نیوز' for improved localization and branding

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="Bob Nouri-M" />
-    <title>{{ $title }} | بهترین سایت خبری</title>
+    <title>{{ $title }} | گربه‌نیوز</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -27,7 +27,7 @@
             height="24"
             class="d-inline-block align-text-top color-light"/> --}}
 
-          بهترین سایت خبری
+          گربه‌نیوز
         </a>
 
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
