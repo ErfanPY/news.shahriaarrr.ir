879e9f37094ffd2af410d28201f674d86a56443d
Erfan Ghofrani <erfan.ghofrani.ch@gmail.com>
2025-07-18 02:38:28 +0330

    Reapply "Fix image source paths in multiple views by adding leading slashes for correct URL resolution"
    This reverts commit 36c055d44effa5eeec4022fd85a5147d7378d693.

--- resources/views/post/my-posts.blade.php
+++ resources/views/post/my-posts.blade.php
@@ -8,7 +8,7 @@
                     <div class="col-md-3 mb-4">
                         <a href="{{ route('post.show', $post->id) }}" class="text-decoration-none">
                             <div class="card product-card h-100">
-                                <img src="{{ $post->url }}" class="card-img-top" alt="تصویر پست">
+                                <img src="/{{ $post->url }}" class="card-img-top" alt="تصویر پست">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $post->name }}</h5>
                                     <p class="card-text text-muted">توضیحات کوتاه خبر</p>
