848ec2ae465e53bc6dd9b50ec2fd069a29d44981
Erfan Ghofrani <erfan.ghofrani.ch@gmail.com>
2025-07-18 02:14:54 +0330

    Correct image source path in post show view by adding leading slash to ensure proper URL resolution

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="image-container">
-                    <img src="{{ $post->url }}" class="img-fluid" />
+                    <img src="/{{ $post->url }}" class="img-fluid" />
                 </div>
                 <div>
                     <h1>{{$post->name}}</h1>
