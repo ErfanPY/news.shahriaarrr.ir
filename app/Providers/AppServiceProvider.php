19129ff37739210ce7dada3280a7d34a3093601e
Erfan Ghofrani <erfan.ghofrani.ch@gmail.com>
2025-07-18 15:10:38 +0330

    fix date 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\JalaliDateService;
 use App\View\Components\Layout;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
@@ -23,5 +24,17 @@ class AppServiceProvider extends ServiceProvider
     {
         // Blade::component('layout', Layout::class);
 
+        // Register global helper functions for Jalali date conversion
+        if (!function_exists('toJalali')) {
+            function toJalali($date, $format = 'Y/m/d') {
+                return JalaliDateService::toJalali($date, $format);
+            }
+        }
+
+        if (!function_exists('toJalaliDiffForHumans')) {
+            function toJalaliDiffForHumans($date) {
+                return JalaliDateService::diffForHumans($date);
+            }
+        }
     }
 }
